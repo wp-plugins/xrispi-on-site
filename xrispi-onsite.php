14677d59e2053101e4f4eae8d65f3ce166c00f69
xrispi <xrispi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-28 08:25:06 +0000

    fixing metadata
    git-svn-id: https://plugins.svn.wordpress.org/xrispi-on-site/tags/1.2@955845 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ xrispi-onsite.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Xrispi On-Site
+ * Plugin URI: https://xrispi.com/
+ * Description: Installs xrispi on-site in your word press
+ * Version: 1.1
+ * Author: Xrispi Labs LTD.
+ * Author URI: https://xrispi.com
+ * License: GPL2
+ */
+
+function xrispi_onsite_wp_head_action() {
+	$output = '<script src="//s.xrispi.com/static/xriscript/xriscript.nocache.js" data-xrispi="{&quot;platform&quot;:&quot;wp&quot;,&quot;v&quot;:&quot;1.1&quot;}"></script>';
+	echo $output;
+}
+
+add_action( 'wp_head', 'xrispi_onsite_wp_head_action');
